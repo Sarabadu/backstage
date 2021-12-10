06a39389ff3d8a04952f6555fcb4e7c602d350f3
Sarabadu <sarabadu@gmail.com>
2021-12-10 22:30:17 +0000

    Deploying to gh-pages from  @ 27e9c706cd7b8af844088187251f43ac526dac40 🚀

---
+++ storybook/react-syntax-highlighter_languages_highlight_bnf.0db08582.iframe.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{3383:function(module,exports){module.exports=function bnf(hljs){return{name:"Backus–Naur Form",contains:[{className:"attribute",begin:/</,end:/>/},{begin:/::=/,end:/$/,contains:[{begin:/</,end:/>/},hljs.C_LINE_COMMENT_MODE,hljs.C_BLOCK_COMMENT_MODE,hljs.APOS_STRING_MODE,hljs.QUOTE_STRING_MODE]}]}}}}]);
